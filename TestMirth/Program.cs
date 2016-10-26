1e225e3c75407f835d11a898b0169e7d1a4faa92
Leonardo Paglionico <paglionico@seminabit.com>
2016-10-26 17:06:31 +0200

    Add project files.

---
+++ TestMirth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
